d854eb8604cb07ffafc93bb804ddbac4c7f82c6a
qiangstrong <qiang20102697@gmail.com>
2015-10-07 22:35:41 +0800

    测试提交
    为协作开发做测试

--- src/test/java/com/subang/controller/ServiceTest.java
+++ src/test/java/com/subang/controller/ServiceTest.java
@@ -1,19 +1,9 @@
 package com.subang.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import weixin.popular.util.JsonUtil;
-
-import com.subang.bean.AddrDetail;
-import com.subang.domain.District;
-import com.subang.domain.Region;
-import com.subang.exception.SuException;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "/applicationContext.xml" })
@@ -21,7 +11,6 @@ public class ServiceTest extends BaseController {
 
 	@Test
 	public void test() {
-		List<AddrDetail> addrDetails=addrDao.findDetailByUserid(9);
 
 		pause();
 	}
